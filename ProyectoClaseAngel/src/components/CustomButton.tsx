8fe05ba9553fc277cbb4c2bef4a435a433e9fbc4
acano2791 <acano2791@gmail.com>
2026-01-26 22:15:28 -0600

    -Commit inicial creando proyecto react native con expo -Agregando componente reutilizable CustomButton.

---
+++ ProyectoClaseAngel/src/components/CustomButton.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function CustomButton (){
+
+    return(
+        <View>
+            <Text> Soy un boton </Text>
+        </View>
+    );
+
+}
