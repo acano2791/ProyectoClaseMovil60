962c320eb752b120c2939bae29ca374a9f68efde
acano2791 <acano2791@gmail.com>
2026-01-28 22:30:13 -0600

    - creacion de componente reutilizabel con props - estilizacion basica de componente button

--- ProyectoClaseAngel/App.tsx
+++ ProyectoClaseAngel/App.tsx
@@ -3,11 +3,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import  CustomButton  from './src/components/CustomButton'
 
 export default function App() {
+const handleOnLogin = () => {
+console.log("Prueba desde app");
+alert("Alerta login desde app");
+
+  }
+
+  const handleOnLogout = () => {
+    alert("Alerta logout desde app");
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App</Text>
       <StatusBar style="auto" />
-      <CustomButton />
+      <CustomButton 
+        title={'Login'} 
+        onClick={handleOnLogin}/>
+      <CustomButton 
+        title={'Salir'} 
+        onClick={handleOnLogout}/>
     </View>
   );
 }
--- ProyectoClaseAngel/src/components/CustomButton.tsx
+++ ProyectoClaseAngel/src/components/CustomButton.tsx
@@ -1,11 +1,38 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function CustomButton (){
+interface CustomButtonProps { 
+    title: string;
+    onClick:()=> void;
+}
+
+export default function CustomButton ({
+    title,
+    onClick
+}: CustomButtonProps){
 
     return(
-        <View>
-            <Text> Soy un boton </Text>
-        </View>
+        
+        <TouchableOpacity 
+        onPress={onClick}
+        style={styles.container}>
+            <Text
+                style={styles.text}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+
     );
 
 }
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:"#2e4566",
+        width: "50%",
+        borderWidth: 2,
+        borderRadius: 8,
+    },
+    text:{
+        color: "white",
+    }
+});
