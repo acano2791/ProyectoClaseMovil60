62cb0c5551b26b11622d46d055000967c341ba37
acano2791 <acano2791@gmail.com>
2026-01-30 22:31:46 -0600

    - finalizando componente reutilizable CustomButton - implementación de operador ternario y estilos condicionados - implementación de explicación de cada linea del codigo App & CustomButton

--- ProyectoClaseAngel/App.tsx
+++ ProyectoClaseAngel/App.tsx
@@ -2,36 +2,54 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  CustomButton  from './src/components/CustomButton'
 
-export default function App() {
-const handleOnLogin = () => {
-console.log("Prueba desde app");
-alert("Alerta login desde app");
-
+export default function App() { /*Componente principal APP */
+const handleOnLogin = () => { /*Funcion para Login */
+console.log("Prueba desde app Login"); /*Imprime en la consola el mensaje que esta entre "" - util para ver si el evento funciona */
+alert("Alerta login desde app"); /*muestra una alerta en el telefono con el mensaje que esta entre "" */
   }
 
-  const handleOnLogout = () => {
-    alert("Alerta logout desde app");
+  const handleOnLogout = () => { /*Funcion para Logout */
+    console.log("Prueba desde app Logout"); /*Imprime en la consola el mensaje que esta entre "" - util para ver si el evento funciona */
+    alert("Alerta logout desde app"); /*muestra una alerta en el telefono con el mensaje que esta entre "" */
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App</Text>
-      <StatusBar style="auto" />
-      <CustomButton 
-        title={'Login'} 
-        onClick={handleOnLogin}/>
-      <CustomButton 
-        title={'Salir'} 
-        onClick={handleOnLogout}/>
+  return (  /*Render (lo que se muestra en la pantalla) */
+    <View style={styles.container}> 
+      <View style={styles.card}>  
+        <Text>Open up App</Text>
+        <StatusBar style="auto" /> 
+        <View style={styles.buttonsWrapper} >
+        <CustomButton 
+         title={'Login'} 
+          onClick={handleOnLogin}/>
+        <CustomButton 
+          title={'Salir'} 
+         onClick={handleOnLogout}
+          variant={'secondary'}/>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create({  /*Crea un objeto de estilos optimizados */
+  container: {  /** */
+    flex: 1, /* ocupa toda la pantalla */
+    alignItems: 'center', /*centra horizontalmente */
+    justifyContent: 'center', /*centra verticalmente */
+  },
+  card:{ /*Tarjeta grande en toda la pantalla */
+    width: "80%", /* 80% de Alto de la pantalla */
+    height: "80%",/* 80% de Ancho en toda la pantalla */
+    borderRadius: 15, /* Bordes redondeados */
+    backgroundColor: '#f3f4f6', /* color gris claro */
+  },
+  buttonsWrapper:{ /* Zona donde van los botoness */
+    backgroundColor: 'green', /* Color del fondo donde van los botones */
+    marginTop: 15, /* Separacion de arriba */
+    height: "30%", /* Ocupa el 30% del alto de la tarjeta */
+    alignItems: "center", /* centra los botones orizontalmente */
+    justifyContent : "center"
+    /*alignContent: "space-around",  consultar al inge cual usar si justifContent o alingnContent */
   },
 });
--- ProyectoClaseAngel/src/components/CustomButton.tsx
+++ ProyectoClaseAngel/src/components/CustomButton.tsx
@@ -1,38 +1,45 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"; /* Iporta StyleSheet, Text, TouchableOpacity, View  */
 
-interface CustomButtonProps { 
-    title: string;
-    onClick:()=> void;
+interface CustomButtonProps {  /*Define que propiedades acepta el componente */
+    title: string; /* Obligatorio texto del boton */
+    onClick:()=> void; /* Obligatorio funcion sin parametros que no retorna nada */
+    variant?: "primary" | "secondary"; /*se le agregaga ? para indicar que la variant no es una variable obligatoria e indica que solo puede ser primaria o secundaria*/
 }
-
-export default function CustomButton ({
+export default function CustomButton ({ /* Componente CustomButton */
     title,
-    onClick
-}: CustomButtonProps){
+    onClick,
+    variant = "primary"  /* variant = "primary" significa: que si no me envian variant, por defecto es "primary" */
+}: CustomButtonProps){  
+    const styles = getstyles(variant); /* Genera estilos en función del variant. */
 
-    return(
-        
+    return(       /* Render del boton */
         <TouchableOpacity 
-        onPress={onClick}
-        style={styles.container}>
+        onPress={onClick}  /* onPress={onClick}: cuando lo presionas, ejecuta la función que mandó el padre. */
+        style={styles.container}> 
             <Text
                 style={styles.text}>
                 {title}
             </Text>
-        </TouchableOpacity>
-
+        </TouchableOpacity> /* Cierra el boton */
     );
-
 }
 
-const styles = StyleSheet.create({
+const getstyles = (variant: 'primary' | 'secondary') =>   /* Funcion que crea estilos dinamicos */
+    StyleSheet.create({
     container:{
-        backgroundColor:"#2e4566",
-        width: "50%",
-        borderWidth: 2,
-        borderRadius: 8,
+        paddingVertical: 15, /* padding arriba y abajo. */
+        alignItems: 'center', /* centra el texto dentro. */
+        width: "80%",         /* el botón mide 80% del ancho de su contenedor padre. */  
+        backgroundColor: /* Operador ternario Si variant es "primary" → fondo azul oscuro. Si no → fondo blanco.: */
+            variant === "primary" ? "#2e4566" : "white" ,  /*el === es una validacion que sea igual a primary el simbolo ? significa si es verdadero se coloca lo que esta dentro de "" y luego : significa si es falso*/
+        borderRadius: 8,  /* Bordes redondeados */
+        borderWidth: 1,   /* Borde de 1px */     
+        borderColor: variant === "primary" ? "#f3f4f6" : "black",  /* Color del borde: primario: gris clarito secundario: negro */
     },
-    text:{
-        color: "white",
+    text:{   /* Estilo de texto */
+        color: 
+            variant === "primary" ? "white" : "black",  /* Color del Texto blanco en botón primario, negro en secundario */
+        fontSize: 18, /* Tamaño 18 */
+        fontWeight: "500"  /* peso medio */
     }
 });
